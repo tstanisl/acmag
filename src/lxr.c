e2a8d198974e4298deeed4eb0e41a8351fbd2f44
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2014-08-31 00:33:30 +0200

    lxr: add lxr_create() and lxr_destory()

---
+++ src/lxr.c
@@ -0,0 +1,37 @@
+#include "lxr.h"
+#include "debug.h"
+
+#include <stdlib.h>
+
+/**
+ * struct lxr - LeXeR object
+ * @file - input stream with source code
+ * @line - current line in source code
+ * @size - capacity of a buffer including 0-term
+ * @data - token content
+ */
+struct lxr {
+        FILE *file;
+	int line;
+	int size;
+	char data[];
+};
+
+struct lxr *lxr_create(FILE *file, int max_token_size)
+{
+	struct lxr *lxr = malloc(sizeof (*lxr) + max_token_size);
+
+	if (ERR_ON(!lxr, "malloc() failed"))
+		return NULL;
+
+	lxr->line = 1;
+	lxr->size = max_token_size;
+	lxr->file = file;
+
+	return lxr;
+}
+
+void lxr_destory(struct lxr *lxr)
+{
+	free(lxr);
+}
