060a99920f42f55fb6df4b4946b76b6df6e4511e
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2014-09-17 16:39:57 +0200

    vec: initial commit

---
+++ src/vec.c
@@ -0,0 +1,96 @@
+#include "vec.h"
+#include "common.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct vec {
+	int used;
+	int capacity;
+	int item_size;
+	char data[];
+};
+
+void *vec_create(int item_size, int min_capacity)
+{
+	int capacity = 8;
+	while (capacity < min_capacity)
+		capacity *= 2;
+
+	struct vec *v = malloc(sizeof (*v) + item_size * capacity);
+	if (!v)
+		return NULL;
+
+	v->used = 0;
+	v->capacity = capacity;
+	v->item_size = item_size;
+
+	printf("create capacity is %d\n", v->capacity);
+
+	return v->data;
+}
+
+void vec_destroy(void *ptr)
+{
+	if (!ptr)
+		return;
+	struct vec *v = container_of(ptr, struct vec, data);
+	free(v);
+}
+
+int vec_size(void *ptr)
+{
+	struct vec *v = container_of(ptr, struct vec, data);
+	return v->used;
+}
+
+int vec_resize(void **ptr, int new_size)
+{
+	struct vec *v = container_of(*ptr, struct vec, data);
+	if (new_size <= v->capacity) {
+		v->used = new_size;
+		return 1;
+	}
+
+	void *new_ptr = vec_create(v->item_size, new_size);
+	if (!new_ptr)
+		return 0;
+
+	struct vec *new_v = container_of(new_ptr, struct vec, data);
+
+	printf("resize to %d\n", new_v->capacity);
+
+	memcpy(new_v->data, v->data, v->item_size * v->used);
+	free(v);
+	new_v->used = new_size;
+	*ptr = new_v->data;
+
+	return 1;
+}
+
+void vec_test(void)
+{
+	int *tab = vec_create(sizeof tab[0], 0);
+
+	for (int j = 0; j < 30; ++j) {
+		printf("vsize=%d   tab={", vec_size(tab));
+		for (int i = 0; i < vec_size(tab); ++i)
+			printf(" %d", tab[i]);
+		printf("}\n");
+		VEC_PUSH(tab, j * j);
+	}
+	vec_destroy(tab);
+
+	struct XY { int x, y; } xy = { 1, 2};
+	struct XY *xytab = vec_create(sizeof xytab[0], 0);
+
+	for (int j = 0; j < 30; ++j) {
+		printf("vsize=%d   tab={", vec_size(xytab));
+		for (int i = 0; i < vec_size(xytab); ++i)
+			printf(" %p", (void*)&xytab[i]);
+		printf("}\n");
+		VEC_PUSH(xytab, xy);
+	}
+	vec_destroy(xytab);
+}
---
+++ src/vec.h
@@ -0,0 +1,13 @@
+#ifndef VEC_H
+#define VEC_H
+
+void *vec_create(int item_size, int min_capacity);
+void vec_destroy(void *vec);
+int vec_size(void *vec);
+int vec_resize(void **pvec, int new_size);
+
+#define VEC_PUSH(vec,item) \
+	(vec_resize((void**)&vec, vec_size(vec) + 1) ? \
+	vec[vec_size(vec) - 1] = item, 1 : 0)
+
+#endif /* VEC_H */
