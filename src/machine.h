17e070a8061a415173156ef727e39bd42ea66b77
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2014-10-12 16:06:09 +0200

    value: move object API here

--- src/machine.h
+++ src/machine.h
@@ -20,21 +20,4 @@ struct acs_user_function {
 
 int register_user_function(struct acs_user_function *ufunc, char *name);
 
-struct acs_object;
-typedef void (*acs_object_dtor_cb)(struct acs_object *obj);
-struct acs_field;
-struct acs_object {
-	struct acs_field *fields;
-	acs_object_dtor_cb dtor;
-	int refcnt;
-};
-
-int object_init(struct acs_object *obj, acs_object_dtor_cb cb);
-struct acs_object *object_get(struct acs_object *object);
-void object_put(struct acs_object *object);
-struct acs_value *object_get_field(struct acs_object *obj,
-	struct acs_value *key);
-struct acs_value *object_try_field(struct acs_object *obj,
-	struct acs_value *key);
-
 #endif /* MACHINE_H */
--- src/value.h
+++ src/value.h
@@ -30,6 +30,23 @@ struct acs_value {
 	} u;
 };
 
+struct acs_object;
+typedef void (*acs_object_dtor_cb)(struct acs_object *obj);
+struct acs_field;
+struct acs_object {
+	struct acs_field *fields;
+	acs_object_dtor_cb dtor;
+	int refcnt;
+};
+
+int object_init(struct acs_object *obj, acs_object_dtor_cb cb);
+struct acs_object *object_get(struct acs_object *object);
+void object_put(struct acs_object *object);
+struct acs_value *object_get_field(struct acs_object *obj,
+	struct acs_value *key);
+struct acs_value *object_try_field(struct acs_object *obj,
+	struct acs_value *key);
+
 /* Simple interface for variable maps */
 struct acs_varmap {
 	struct acs_var *head;
