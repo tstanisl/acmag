12e2ec485735c126af0051f975851dcbe873188a
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2014-10-19 18:40:06 +0200

    machine: add machine API

--- src/machine.h
+++ src/machine.h
@@ -1,7 +1,22 @@
 #ifndef MACHINE_H
 #define MACHINE_H __FILE__
 
-#include "syntax.h"
+#include "cstr.h"
 #include "value.h"
+
+int acs_call(struct acs_value *val, int argin, int argout);
+int acs_call_by_name(char *fname, int argin, int argout);
+
+void acs_push_num(float nval);
+void acs_push_cstr(char *str);
+void acs_push_str(struct str *sval);
+
+const struct acs_value *acs_argv(int arg);
+#define acs_arg_num(arg) value_to_num(acs_argv(arg))
+#define acs_arg_str(arg) value_to_str(acs_argv(arg))
+int acs_argc(void);
+
+struct str *acs_pop_str(void);
+float acs_pop_num(void);
 
 #endif /* MACHINE_H */
