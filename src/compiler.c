02c7d2b7d9ed4aad4f89795b32eaad2f6e9b465f
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2014-10-24 23:31:30 +0200

    compiler: initial commit

---
+++ src/compiler.c
@@ -0,0 +1,11 @@
+#include "debug.h"
+#include "function.h"
+#include "value.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct acs_finstance *acs_compile_file(FILE *f, char *path)
+{
+	return NULL;
+}
