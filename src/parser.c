593d83f4244cc80439062bcc02f6d9a521ae47aa
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2016-01-30 15:04:34 +0100

    parser: initial

---
+++ src/parser.c
@@ -0,0 +1,32 @@
+#include "debug.h"
+#include "lxr.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+static enum token cur;
+static struct lxr *lxr;
+
+static int perr(char *fmt, ...)
+{
+	va_list va;
+
+	va_start(va, fmt);
+	printf("%d: error: ", lxr_line(lxr));
+	vprintf(fmt, va);
+	puts("");
+	va_end(va);
+	cur = TOK_ERR;
+	return -1;
+}
+
+static void consume(void)
+{
+	CRIT_ON(cur == TOK_ERR, "consuming error");
+	cur = lxr_get(lxr);
+}
+
+#define accept(c) ((c == cur) ? consume(), 1 : 0)
+
+void parse_test(void)
+{
+}
