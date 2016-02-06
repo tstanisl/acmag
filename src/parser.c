48d79018a0251f3f368eca5deabfb94969ee2e3b
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2016-02-06 16:32:50 +0100

    parser: add containers for indexed objects

--- src/parser.c
+++ src/parser.c
@@ -1,9 +1,45 @@
 #include "debug.h"
 #include "lxr.h"
 #include "list.h"
+#include "str.h"
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
+
+#define MAX_IDS 256
+static struct str *const_str[MAX_IDS];
+static int n_const_str;
+static int const_num[MAX_IDS];
+static int n_const_num;
+static struct str *vars[MAX_IDS];
+static int n_vars;
+
+static int var_get(char *s)
+{
+	for (int i = 0; i < n_vars; ++i)
+		if (strcmp(s, vars[i]->str) == 0)
+			return str_get(vars[i]), i;
+	vars[n_vars++] = str_create(s);
+	return n_vars - 1;
+}
+
+static int const_str_get(char *s)
+{
+	for (int i = 0; i < n_const_str; ++i)
+		if (strcmp(s, const_str[i]->str) == 0)
+			return str_get(const_str[i]), i;
+	const_str[n_const_str++] = str_create(s);
+	return n_const_str - 1;
+}
+
+static int const_num_get(int v)
+{
+	for (int i = 0; i < n_const_num; ++i)
+		if (v == const_num[i])
+			return i;
+	const_num[n_const_num++] = v;
+	return n_const_num - 1;
+}
 
 static enum token cur;
 static struct lxr *lxr;
