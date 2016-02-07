35e2bf5463356d71ea3069ca3a46d6fa1a09f579
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2016-02-07 18:04:32 +0100

    list: add list_empty(), list_splice(), list_splice_tail()

--- src/list.h
+++ src/list.h
@@ -38,6 +38,35 @@ static inline void list_init(struct list *node)
 	node->prev = node;
 }
 
+static inline int list_empty(struct list *head)
+{
+	return head == head->next;
+}
+
+static inline void __list_splice(struct list *head,
+	struct list *prev, struct list *next)
+{
+	struct list *first = head->next;
+	struct list *last = head->prev;
+
+	prev->next = first;
+	first->prev = prev;
+	last->next = next;
+	next->prev = last;
+}
+
+static inline void list_splice(struct list *ohead, struct list *nhead)
+{
+	if (!list_empty(ohead))
+		__list_splice(ohead, nhead, nhead->next);
+}
+
+static inline void list_splice_tail(struct list *ohead, struct list *nhead)
+{
+	if (!list_empty(ohead))
+		__list_splice(ohead, nhead->prev, nhead);
+}
+
 #define DEFINE_LIST(name) struct list name = { .next = &name, .prev = &name }
 
 #define list_foreach(it,head) \
